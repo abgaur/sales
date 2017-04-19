e86e866b3ab2890b462d52e6c1eb1eef38156853
Amey <ameyparab2007@gmail.com>
2017-04-18 17:22:30 -0700

    Initial changes for edit popup for reminder date

--- angular-src/src/app/components/popup/popup.component.ts
+++ angular-src/src/app/components/popup/popup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges, Input, Output, EventEmitter } from '@angular/core';
 import { ClientService } from '../../services/client.service';
 import 'eonasdan-bootstrap-datetimepicker';
 import * as moment from 'moment'
@@ -8,7 +8,7 @@ import * as moment from 'moment'
   templateUrl: './popup.component.html',
   styleUrls: ['./popup.component.css']
 })
-export class PopupComponent implements OnInit {
+export class PopupComponent implements OnInit, OnChanges {
 
   @Input() selectedClient;
   @Output() clientUpdated: EventEmitter<any> = new EventEmitter();
@@ -18,12 +18,26 @@ export class PopupComponent implements OnInit {
     ignoreReadonly: true
   }
   private reminderDate = moment();
-  private newComment = '';
+  private reminderTitle: String;
   private isReminderEnabled = false;
+  private newComment = '';
 
   constructor(private clientService: ClientService) {}
 
   ngOnInit() {}
+
+  ngOnChanges(changes: SimpleChanges) {
+    if(this.selectedClient){
+        this.isReminderEnabled = !!this.selectedClient.reminderDate;
+        if(this.selectedClient.reminderDate){
+          this.reminderDate = moment(this.selectedClient.reminderDate);
+          this.reminderTitle = this.selectedClient.reminderTitle;
+        }else{
+          this.reminderDate = moment();
+          this.reminderTitle = '';
+        }
+    }
+  }
 
   dateChange(date) {
     this.reminderDate = date;
@@ -39,9 +53,7 @@ export class PopupComponent implements OnInit {
   }
 
   updateClient(){
-  	//console.log(this.selectedClient);
     this.selectedClient.reminderDate = this.isReminderEnabled ? this.reminderDate.toString() : null;
-    // console.log(this.reminderDate);
   	this.clientService.updateClient(this.selectedClient).subscribe((data) => {
         this.clientUpdated.emit(data);
     });
@@ -50,6 +62,5 @@ export class PopupComponent implements OnInit {
   clearReminder() {
     this.isReminderEnabled = !this.isReminderEnabled;
   }
-
 
 }
