181409bf95fc6f93dd3dca592e89e0ea4e54b828
psalunke <psalunke@etouch.net>
2017-05-02 11:20:12 -0700

    mytask popup completed

--- angular-src/src/app/services/client.service.ts
+++ angular-src/src/app/services/client.service.ts
@@ -35,4 +35,13 @@ export class ClientService {
 		comment.user = JSON.parse(localStorage.getItem('user'));
   		return this.http.post(url, comment).map(res => res.json());
 	}
+
+	updateProfile(user:Object){
+		console.log(user);
+		const url = "http://localhost:3000/users/profile";
+		const username = JSON.parse(localStorage.getItem('user')).username;
+		const headers = new Headers();
+	  	headers.append('Content-type', 'application/json');
+  		return this.http.post(url, user, { headers: headers }).map(res => res.json());	
+	}
 }
