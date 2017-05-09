79b47c62d64d626f84c391422a37a50afe58e289
Amey <ameyparab2007@gmail.com>
2017-05-09 15:55:04 -0700

    used static json to display the data. Need to integrate with API

--- angular-src/src/app/components/meetings-scheduled/meetings-scheduled.component.ts
+++ angular-src/src/app/components/meetings-scheduled/meetings-scheduled.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DashboardService } from '../../services/dashboard.service';
 
 @Component({
   selector: 'app-meetings-scheduled',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./meetings-scheduled.component.css']
 })
 export class MeetingsScheduledComponent implements OnInit {
-
-  constructor() { }
+	isLoading: Boolean = false;
+	meetings: Array<any> = [];
+	monthCounts: Array<any> = [];
+	
+  constructor(private dashboardService: DashboardService) { }
 
   ngOnInit() {
+  	this.isLoading = true;
+  	this.dashboardService.getMeetingsScheduled().then((data) => {
+  			this.isLoading = false;
+			this.meetings = data.clients;
+  		}).catch((err) => {
+  			this.isLoading = false;
+  			console.log(err);
+  		});
+
+  	this.dashboardService.getMeetingsCount().then((data) => {
+  			this.isLoading = false;
+			this.monthCounts = data.counts;
+  		}).catch((err) => {
+  			this.isLoading = false;
+  			console.log(err);
+  		});
   }
 
 }
--- angular-src/src/app/services/dashboard.service.ts
+++ angular-src/src/app/services/dashboard.service.ts
@@ -23,4 +23,88 @@ export class DashboardService {
     return this.http.post(salesDataUrl, assignToBody, {headers: headers})
       .map(res => res.json());
   }
+
+  getMeetingsCount(){
+    var data = {
+      counts: [
+        {
+          "month": "Apr",
+          "count": 3
+        },
+        {
+          "month": "Mar",
+          "count": 4
+        },
+        {
+          "month": "Feb",
+          "count": 1
+        }
+      ]
+    }
+
+    return new Promise<any>((resolve, reject) => {
+      setTimeout(() => resolve(data), 3000);
+    });
+  }
+
+  getMeetingsScheduled() {
+
+    var data = {
+      clients: [
+        {
+          "company": "Symantec",
+          "name": "Mark  Sherwood",
+          "isr": "ISR",
+          "bdm": "BDM"
+        },
+        {
+          "company": "Symantec",
+          "name": "Andrew  Gardner",
+          "isr": "ISR",
+          "bdm": "BDM"
+        },
+        {
+          "company": "Symantec",
+          "name": "Mannie  Heer",
+          "isr": "ISR",
+          "bdm": "BDM"
+        },
+        {
+          "company": "Symantec",
+          "name": "Andrew  Gardner",
+          "isr": "ISR",
+          "bdm": "BDM"
+        },
+        {
+          "company": "Symantec",
+          "name": "Mannie  Heer",
+          "isr": "ISR",
+          "bdm": "BDM"
+        },
+        {
+          "company": "Symantec",
+          "name": "Andrew  Gardner",
+          "isr": "ISR",
+          "bdm": "BDM"
+        },
+        {
+          "company": "Symantec",
+          "name": "Mannie  Heer",
+          "isr": "ISR",
+          "bdm": "BDM"
+        }
+      ]
+      
+    }
+
+    return new Promise<any>((resolve, reject) => {
+      setTimeout(() => resolve(data), 3000);
+    });
+
+    /*var salesDataUrl = 'http://localhost:3000/clients/data/';
+    let headers =  new Headers();
+    headers.append('Content-type', 'application/json');
+    return this.http.get(salesDataUrl)
+      .map(res => res.json());*/
+  }
 }
