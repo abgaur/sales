fea34cd5d0aca931015672b23586670526b22227
abgaur <abhishekgaur61@gmail.com>
2017-05-22 11:34:39 -0700

    added authentication

--- angular-src/src/app/services/dashboard.service.ts
+++ angular-src/src/app/services/dashboard.service.ts
@@ -3,34 +3,37 @@ import {Http, Headers} from '@angular/http';
 import 'rxjs/add/operator/map';
 import { tokenNotExpired } from 'angular2-jwt';
 import { environment } from '../../environments/environment';
+import { AuthService } from './auth.service';
 
 @Injectable()
 export class DashboardService {
 
-  constructor(private http: Http) { }
+  headers: any;
+  	constructor(private http: Http,
+	  private authService: AuthService) {
+		  this.authService.loadToken();
+		  this.headers = new Headers();
+		  this.headers.append('Content-type', 'application/json');
+		  this.headers.append('Authorization', this.authService.authToken);		 
+	  }
 
    getSalesData() {
     var role = JSON.parse(localStorage.getItem('user')).role;
     var salesDataUrl = environment.baseUrl+'clients/data/'+role;
-    let headers =  new Headers();
-    headers.append('Content-type', 'application/json');
-    return this.http.get(salesDataUrl)
+   
+    return this.http.get(salesDataUrl, {headers: this.headers})
       .map(res => res.json());
   }
 
    setAssignedTo(assignToBody) {
     var salesDataUrl = environment.baseUrl+'clients/assignto';
-    let headers =  new Headers();
-    headers.append('Content-type', 'application/json');
-    return this.http.post(salesDataUrl, assignToBody, {headers: headers})
+    return this.http.post(salesDataUrl, assignToBody, {headers: this.headers})
       .map(res => res.json());
   }
 
   getMeetingsCount(){
     var url = environment.baseUrl+'teamdata/meetingsscheduled';
-    let headers =  new Headers();
-    headers.append('Content-type', 'application/json');
-    return this.http.get(url, { headers: headers })
+    return this.http.get(url, { headers: this.headers })
       .map(res => {
         let counts = res.json();
         let currentMonth = counts.shift();
@@ -41,9 +44,7 @@ export class DashboardService {
 
   getTopUsers(){
     var url = environment.baseUrl+'teamdata/topusers';
-    let headers =  new Headers();
-    headers.append('Content-type', 'application/json');
-    return this.http.get(url, { headers: headers })
+    return this.http.get(url, { headers: this.headers })
       .map(res => res.json());
   }
 
--- angular-src/src/app/services/reminders.service.ts
+++ angular-src/src/app/services/reminders.service.ts
@@ -2,11 +2,19 @@ import { Injectable } from '@angular/core';
 import {Http, Headers} from '@angular/http';
 import 'rxjs/add/operator/map';
 import { environment } from '../../environments/environment';
+import { AuthService } from './auth.service';
 
 @Injectable()
 export class RemindersService {
 
-  constructor(private http: Http) { }
+   headers: any;
+  	constructor(private http: Http,
+	  private authService: AuthService) {
+		  this.authService.loadToken();
+		  this.headers = new Headers();
+		  this.headers.append('Content-type', 'application/json');
+		  this.headers.append('Authorization', this.authService.authToken);		 
+	  }
 
    getReminders() {
     let assignTo = JSON.parse(localStorage.getItem('user')).email;
@@ -16,9 +24,7 @@ export class RemindersService {
     endDate.setHours(23,59,59);
     
     let remindersUrl = environment.baseUrl+'clients/reminders/'+assignTo+'/'+startDate+'/'+endDate;
-    let headers =  new Headers();
-    headers.append('Content-type', 'application/json');
-    return this.http.get(remindersUrl)
+    return this.http.get(remindersUrl, {headers: this.headers})
       .map(res => res.json());
   }
 
